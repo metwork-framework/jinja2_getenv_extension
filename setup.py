f9ef91128534c4262f30ab2d242b20cbed5cf376
Fabien MARTY <fabien.marty@gmail.com>
2020-01-22 10:25:35 +0100

    build: better pypi publishing

--- setup.py
+++ setup.py
@@ -7,8 +7,18 @@ with open('requirements.txt') as reqs:
 
 setup(
     name="jinja2_getenv_extension",
-    version="0.0.1",
+    version="0.0.2",
     license="BSD",
     packages=find_packages(),
-    install_requires=install_requires
+    install_requires=install_requires,
+    python_requires='>=3.0',
+    description="a jinja2 extension to access to system environment variables",
+    url="https://github.com/metwork-framework/jinja2_getenv_extension",
+    keywords="jinja2 extension",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3"
+    ]
 )
