1b976d7010abb583e5b43069f1dabd4eef46ce28
wwuck <301402+wwuck@users.noreply.github.com>
2021-10-01 17:23:45 +1000

    fix: replace deprecated evalcontextfilter (#9)
    evalcontextfilter is deprecated and will be removed in Jinja 3.1
    replace it with pass_eval_context

--- jinja2_getenv_extension/__init__.py
+++ jinja2_getenv_extension/__init__.py
@@ -1,9 +1,14 @@
 import os
+
 import jinja2
 from jinja2.ext import Extension
 
+try:
+    from jinja2 import pass_eval_context as eval_context
+except ImportError:
+    from jinja2 import evalcontextfilter as eval_context
 
-@jinja2.evalcontextfilter
+@eval_context
 def getenv(eval_ctx, value, default=None):
     result = os.environ.get(value, default)
     if result is None:
