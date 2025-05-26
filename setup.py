3a5741890b343c0b16ed98c20acdb6c3a9953661
thebaptiste <jean-baptiste.veslin@meteo.fr>
2025-05-26 17:15:49 +0000

    build: add pyproject.toml to remove deprecation warning at install

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author="Fabien MARTY",
     author_email="fabien.marty@gmail.com",
     name="jinja2_getenv_extension",
-    version="1.0.2",
+    version="1.0.3",
     license="BSD",
     packages=find_packages(),
     install_requires=install_requires,
@@ -25,7 +25,6 @@ setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3"
     ]
 )
